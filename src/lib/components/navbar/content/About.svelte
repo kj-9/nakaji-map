6d6848cc68a26871ec3a738a63e8dcd82fd322aa
Kj <46800811+kj-9@users.noreply.github.com>
2023-12-10 19:54:21 +0900

    added link to issue

--- src/lib/components/navbar/content/About.svelte
+++ src/lib/components/navbar/content/About.svelte
@@ -3,7 +3,10 @@
 		<div class="font-semibold">中島浩二チャンネルマップ</div>
 		<p class="mt-2 leading-relaxed text-gray-500">
 			<a href="https://www.youtube.com/@user-uw6md7xe9l">中島浩二チャンネル</a>のファンサイトです。
-			動画で紹介されたお店を地図上に表示しています。 情報には誤りがある可能性があります。
+			動画で紹介されたお店を地図上に表示しています。
+			お店の名前や位置情報に誤りがある可能性があります。誤りの指摘は<a
+				href="https://github.com/kj-9/nakaji-map/issues/new">GithubのIssue</a
+			>からお願いします。
 		</p>
 	</li>
 </ul>
