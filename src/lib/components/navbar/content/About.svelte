847aff21c19c2802601afeed83245efae5c0dbf2
Kj <46800811+kj-9@users.noreply.github.com>
2024-01-10 01:07:21 +0900

    update about dialog

--- src/lib/components/navbar/content/About.svelte
+++ src/lib/components/navbar/content/About.svelte
@@ -1,10 +1,17 @@
 <ul class="text-left">
 	<li class="text-xs sm:text-base">
 		<div class="font-semibold">中島浩二チャンネルマップ</div>
-		<p class="mt-2 leading-relaxed text-gray-500">
-			<a href="https://www.youtube.com/@user-uw6md7xe9l">中島浩二チャンネル</a>のファンサイトです。
-			動画で紹介されたお店を地図上に表示しています。
-			お店の名前や位置情報に誤りがある可能性があります。誤りの指摘は<a
+		<p class="mt-2 leading-relaxed text-gray-800">
+			本サイトは<a href="https://www.youtube.com/@user-uw6md7xe9l">中島浩二チャンネル</a
+			>のファンサイトです。 動画で紹介されたお店を地図上に表示しています。
+		</p>
+		<p class="mt-2 leading-relaxed text-gray-800">
+			中島浩二さんは通称ナカジーと呼ばれる福岡を中心に活動されているタレントです。YouTubeチャンネルでは福岡の美味しいお店を紹介されています。
+		</p>
+
+		<div class="mt-4 font-semibold">利用上の注意</div>
+		<p class="mt-2 leading-relaxed text-gray-800">
+			お店の名前や位置情報に誤りがある可能性があります。誤りのご指摘・修正のご依頼は<a
 				href="https://github.com/kj-9/nakaji-map/issues/new">GithubのIssue</a
 			>からお願いします。
 		</p>
