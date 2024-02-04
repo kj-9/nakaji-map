74db5e1ed35af2e87b629d1cf7a9f3e4de8b1955
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-04 13:21:36 +0900

    add 404

---
+++ src/routes/404/+page.svelte
@@ -0,0 +1,7 @@
+<!-- for ssg to generate ./404.html. see also: https://qiita.com/irohafox/items/53e356b10a23123a1f4f -->
+
+<script lang="ts">
+	import ErrorPage from '../+error.svelte';
+</script>
+
+<ErrorPage />
