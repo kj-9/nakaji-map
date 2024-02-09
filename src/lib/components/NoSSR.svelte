ce340af19135eda8071ccd3208daca21660176b4
Kj <46800811+kj-9@users.noreply.github.com>
2024-02-09 21:12:25 +0900

    add fallback

---
+++ src/lib/components/NoSSR.svelte
@@ -0,0 +1,6 @@
+<!-- see https://www.youtube.com/watch?v=RT0aemVpvVE -->
+{#await Promise.resolve()}
+    <slot name="fallback" />
+{:then}
+    <slot />
+{/await}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,8 +5,13 @@
 
 	import Map from '$lib/components/MapPane.svelte';
 	import NavBar from '$lib/components/navbar/NavBar.svelte';
+	import NoSSR from '$lib/components/NoSSR.svelte';
 	import { geodata } from '$lib/store';
 	import type { FeatureForPopup } from '$lib/store';
+
+	import About from '$lib/components/navbar/content/About.svelte'
+	import News from '$lib/components/navbar/content/News.svelte'
+	import SearchTable from '$lib/components/navbar/content/SearchTable.svelte';
 
 	export let initialFlyFeature: FeatureForPopup;
 
@@ -37,10 +42,19 @@
 	<link rel="canonical" href="https://nakaji-map.kj-dev.net/" />
 </svelte:head>
 
+<NoSSR>
 <div class="relative">
 	<Map {initialFlyFeature} />
-
 	<div class="absolute top-5 left-5">
 		<NavBar showAbout={!initialFlyFeature} />
 	</div>
+
 </div>
+
+<svelte:fragment slot="fallback">
+	<About/>
+	(注意) 地図を表示するにはJavascriptを有効にしてください。
+	<News/>
+	<SearchTable/>
+</svelte:fragment>
+</NoSSR>
